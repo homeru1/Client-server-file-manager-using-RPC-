e29e74895d95cd264939fb86f5c57ad0e7592ea5
homeru1 <71785696+homeru1@users.noreply.github.com>
2023-06-03 17:30:05 +0300

    Add files via upload

---
+++ RPCClient.cpp
@@ -0,0 +1,233 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <windows.h>
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include "Source_h.h"
+#include <locale.h>
+#include <conio.h>
+#include <cstdio>
+
+#pragma comment (lib, "Rpcrt4.lib")
+using namespace std;
+
+#define SIZE 100
+#define MAX_BUFF 1048576
+
+int BUFFER[MAX_BUFF];
+
+int GetName(char* path) {
+	int i = 0,j=0;
+	while (path[i]!='\0') {
+		if (path[i] == '\\')j = i;
+		i++;
+	}
+	return j;
+}
+
+void REmove(int id) {
+	char path[512];// = "C:\\Users\\vaasa\\source\\repos\\RPCServer\\RPCServer\\test.txt";
+	cout << "Please enter the path to the file:";
+	cin >> path;
+	int state = Remove((const unsigned char*)path, id);
+	if (state == 1) {
+		cout << "Done!" << endl;
+	}
+	else if (state == -1) {
+		cout << "Impersonation error" << endl;
+		return;
+	}
+	else if (state == -2) {
+		cout << "Not enough privileges" << endl;
+		return;
+	}
+}
+
+void DOwnload(int id) {
+	char path[512];// = "C:\\Users\\vaasa\\source\\repos\\RPCServer\\RPCServer\\test.txt";
+	cout << "Please enter the path to the file:";
+	cin >> path;
+	int tmp = GetName(path);
+	char name[128];
+	strcpy(name, &path[tmp + 1]);
+	int count = 1024, result = 1;
+	int buffer[1024];
+	count = Download(buffer, (const unsigned char*)path, id);
+	if (count == -1) {
+		cout << "Impersonation error" << endl;
+		return;
+	}
+	else if (count == -2) {
+		cout << "Access denied" << endl;
+		return;
+	}
+	FILE* open = fopen(name, "w");
+	fwrite(buffer, sizeof(int), count, open);
+	while (count == 1024) {
+		count = Download(buffer, (const unsigned char*)path, id);
+			fwrite(buffer, sizeof(int), count, open);
+	}
+	fclose(open);
+}
+
+void UPload(int id) {
+	char path[512];// = "C:\\Users\\vaasa\\source\\repos\\RPCClient\\RPCClient\\test.txt";
+	char dest[512];
+	cout << "Please enter the path to the file:";
+	cin >> path;
+	cout << "Please enter the destination of the file:";
+	cin >> dest;
+	FILE* open = fopen(path, "r");
+	int tmp = GetName(path);
+	char name[128];
+	strcpy(name, &path[tmp]);
+	char buffer[1024];
+	bool uploading = 1;
+	int count = 1024, result = 1, size = 0;
+	char a[] = "\\";
+	strcat(dest, a);
+	strcat(dest, name);
+	while (count==1024) {
+		count = fread(buffer, 1, 1024, open);
+		result = Upload((const unsigned char*)buffer, (const unsigned char*)dest, count, size,id);
+		if (result == -1) {
+			cout << "Impersonation error" << endl;
+			fclose(open);
+			return;
+		}
+		else if (result == -2) {
+			cout << "Access denied" << endl;
+			fclose(open);
+			return;
+		}
+		size += count;
+	}
+	//Upload((const unsigned char*)buffer, (const unsigned char*)name, 1, -1, id);
+	fclose(open);
+}
+
+int main() {
+	setlocale(LC_ALL, "Rus");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	RPC_STATUS status;
+	RPC_CSTR szStringBinding = NULL;
+
+	//Функция RpcStringBindingCompose создает дескриптор привязки строки.
+	char IP[32];
+	char Port[8];
+	//cout << "IP: ";
+	//cin >> IP;
+	//cout << "Port: ";
+	//cin >> Port;
+	status = RpcStringBindingComposeA(
+		NULL,                                        // UUID to bind to.
+		(RPC_CSTR)("ncacn_ip_tcp"),					// Use TCP/IP protocol.
+		(RPC_CSTR)("192.168.31.69"),				// TCP/IP network address to use.  // 192.168.31.69  localhost
+		(RPC_CSTR)("9000"),					// TCP/IP port to use.
+		NULL,										// Protocol dependent network options to use.
+		&szStringBinding);							// String binding output.
+
+	if (status)
+	{
+		cout << "Connect error." << endl;
+		exit(status);
+	}
+
+	//Функция RpcBindingFromStringBinding возвращает дескриптор привязки из строкового представления дескриптора привязки.
+	status = RpcBindingFromStringBindingA(
+		szStringBinding,        // The string binding to validate.
+		&hRPCBinding);     // Put the result in the implicit binding
+								// handle defined in the IDL file.
+	if (status)
+		exit(status);
+	char login[100] = "\0";
+	char password[100] = "\0";
+	int state = -1;//-1;
+	while (state<0) {
+		cout << "login: ";
+		cin >> login;
+		cout << "Password: ";
+		cin >> password;
+		cout << "Entering the server...: ";
+		state = Log_in((const unsigned char*)login, (const unsigned char*)password);
+		if (state == -1) {
+			cout << "Too many users, what a bit and try again" << endl;
+			Sleep(100);
+		}
+		else if(state == -2) {
+			cout << "Incorrect username or password try again" << endl;
+			cout << "login: ";
+			cin >> login;
+			cout << "Password: ";
+			cin >> password;
+		}
+		else if (state == -3) {
+			cout << "Impersonation error" << endl;
+			cout << "login: ";
+			cin >> login;
+			cout << "Password: ";
+			cin >> password;
+		}
+	}
+	system("cls");
+	cout << "Enter is succesful!, Hello " << login << endl;
+	int tmp = 1;
+	RpcTryExcept
+	{
+		while (tmp!=4) {
+			cout << "What to do: \n1)Upload file \n2)Download file\n3)Delete file from the server\n4)Exit";
+			cin >> tmp;
+			system("cls");
+			switch (tmp)
+			{
+			case 1:
+				UPload(state);
+				break;
+			case 2:
+				DOwnload(state);
+				break;
+			case 3:
+				REmove(state);
+				break;
+			case 4:
+			break;
+			default:
+			break;
+			}
+		}
+		Output((const unsigned char*)"Hello RPC World!");
+	}
+		RpcExcept(1)
+	{
+		std::cerr << "Runtime reported exception " << RpcExceptionCode()
+			<< std::endl;
+	}
+	RpcEndExcept
+
+		// Free the memory allocated by a string.
+		status = RpcStringFreeA(
+			&szStringBinding); // String to be freed.
+
+	if (status)
+		exit(status);
+
+	// Releases binding handle resources and disconnects from the server.
+	status = RpcBindingFree(
+		&hRPCBinding); // Frees the implicit binding handle defined in the IDL file.
+
+	if (status)
+		exit(status);
+}
+
+void* __RPC_USER midl_user_allocate(size_t size)
+{
+	return malloc(size);
+}
+
+// Memory deallocation function for RPC.
+void __RPC_USER midl_user_free(void* p)
+{
+	free(p);
+}
---
+++ RPCServer.cpp
@@ -0,0 +1,168 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include "Source_h.h"
+#include <fstream>
+#include <windows.h>
+#include <stdio.h>
+
+using namespace std;
+
+#pragma comment (lib, "Rpcrt4.lib")
+
+struct Client {
+	char login[100];
+	char password[100];
+	bool access = 0;
+	FILE* working = NULL;
+	handle_t id = 0;
+};
+
+struct Client Clients[100];
+
+int g_amount_of_clinets = 0;
+
+void Output(/* [string][in] */ const unsigned char* szOutput)
+{
+	cout << szOutput << std::endl;
+}
+
+
+int Remove(const unsigned char* path, int idd) {
+	if (ImpersonateLoggedOnUser(Clients[idd].id) == 0) {
+		return -1;
+	}
+	if (remove((const char*)path) == -1)
+	{
+		return -2;
+	}
+}
+
+int Download(/* [string][in] */int buf[1024], /* [string][in] */const unsigned char* path, int idd) {
+	if (!ImpersonateLoggedOnUser(Clients[idd].id)) {
+		cout << "Impersonate Error!";
+		return -1;
+	}
+	if (Clients[idd].working == NULL)Clients[idd].working = fopen((const char*)path, "r");
+	if (!Clients[idd].working) {
+		Clients[idd].working = NULL;
+		cout << "Acces Error!";
+		return -2;
+	}
+	int count = fread((int*)buf, sizeof(int), 1024, Clients[idd].working);
+	if (count != 1024) {
+		fclose(Clients[idd].working);
+		Clients[idd].working = NULL;
+	}
+	return count;
+
+}
+
+int Upload(/* [string][in] */const unsigned char* buf, /* [string][in] */const unsigned char* filename, /* [in] */int count, /* [in] */int size, /* [in] */int idd) {
+	if (ImpersonateLoggedOnUser(Clients[idd].id) == 0) {
+		cout << "Impersonate Error!";
+		size = -1;
+	}
+	if (Clients[idd].working == NULL)Clients[idd].working = fopen((const char*)filename, "w");
+	if (!Clients[idd].working) {
+		Clients[idd].working = NULL;
+		cout << "Acces Error!";
+		return -2;
+	}
+	for (int i = 0; i < count; i++) {
+		fputc(buf[i], Clients[idd].working);
+	}
+	if (count != 1024) {
+		fclose(Clients[idd].working);
+		Clients[idd].working = NULL;
+	}
+	return 1;
+}
+
+int Log_in(/* [string][in] */const unsigned char* login,/* [string][in] */ const unsigned char* password) { // -1 too many clients
+	printf("%s %s\n", login, password);
+	if (g_amount_of_clinets > 100)return -1;
+	int current_index = g_amount_of_clinets;
+
+	handle_t handle = 0;
+
+	if (!LogonUserA((LPCSTR)login, NULL, (LPCSTR)password, LOGON32_LOGON_INTERACTIVE, LOGON32_PROVIDER_DEFAULT, &handle))
+	{
+		cout << "Descriptor token error.";
+		return -2;
+	}
+	if (!ImpersonateLoggedOnUser(handle))
+	{
+		cout << "Impersonation error.";
+		return -3;
+	}
+
+	Clients[current_index].id = handle;
+	strcpy(Clients[current_index].login, (char *)login);
+	strcpy(Clients[current_index].password, (char *)password);
+	g_amount_of_clinets++;
+	cout << "Client is on." << endl;
+	return g_amount_of_clinets-1;
+}
+
+RPC_STATUS CALLBACK SecurityCallback(RPC_IF_HANDLE /*hInterface*/, void* /*pBindingHandle*/)
+{
+	return RPC_S_OK; // Always allow anyone.
+}
+
+int main() {
+	/*
+	setlocale(LC_ALL, "Rus");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	*/
+
+	RPC_STATUS status;
+	RpcServerRegisterAuthInfoA(nullptr, RPC_C_AUTHN_WINNT, 0, nullptr);
+	//Функция RpcServerUseProtseqEp сообщает библиотеке времени выполнения RPC использовать указанную \
+	последовательность протокола в сочетании с указанной конечной точкой для приема вызовов удаленных процедур.
+	status = RpcServerUseProtseqEpA(
+		(RPC_CSTR)("ncacn_ip_tcp"),			// Use TCP/IP protocol.
+		RPC_C_PROTSEQ_MAX_REQS_DEFAULT,		// Backlog queue length for TCP/IP.
+		(RPC_CSTR)("9000"),					// TCP/IP port to use.
+		NULL);								// No security.
+
+	if (status)
+		exit(status);
+
+	//Функция RpcServerRegisterIf2 регистрирует интерфейс с библиотекой времени выполнения RPC.
+	status = RpcServerRegisterIf2(
+		RPC_v1_0_s_ifspec,              // Interface to register.
+		NULL,                                // Use the MIDL generated entry-point vector.
+		NULL,                                // Use the MIDL generated entry-point vector.
+		RPC_IF_ALLOW_CALLBACKS_WITH_NO_AUTH, // Forces use of security callback.
+		RPC_C_LISTEN_MAX_CALLS_DEFAULT,      // Use default number of concurrent calls.
+		(unsigned)-1,                        // Infinite max size of incoming data blocks.
+		SecurityCallback);                   // Naive security callback.
+
+	if (status)
+		exit(status);
+	cout << "Server in on" << endl;
+
+	// Функция RpcServerListen сигнализирует библиотеке времени выполнения RPC прослушивать удаленные вызовы процедур
+	status = RpcServerListen(
+		1,                                   // Recommended minimum number of threads.
+		RPC_C_LISTEN_MAX_CALLS_DEFAULT,      // Recommended maximum number of threads.
+		FALSE);                              // Start listening now.
+	cout << "Server is off" << endl;
+	if (status)
+		exit(status);
+}
+
+// Memory allocation function for RPC.
+// The runtime uses these two functions for allocating/deallocating
+// enough memory to pass the string to the server.
+void* __RPC_USER midl_user_allocate(size_t size)
+{
+	return malloc(size);
+}
+
+// Memory deallocation function for RPC.
+void __RPC_USER midl_user_free(void* p)
+{
+	free(p);
+}
